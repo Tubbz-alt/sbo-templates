473589f1ace850bdb1f5b4d68f3cf9e6ebb3c421
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2019-01-23 18:14:29 +0100

    Updated copyright to 2019
    Signed-off-by: Dimitris Zlatanidis <d.zlatanidis@gmail.com>

--- sbo_templates/__metadata__.py
+++ sbo_templates/__metadata__.py
@@ -3,7 +3,7 @@
 
 # __metadata__.py file is part of sbo-templates.
 
-# Copyright 2015-2018 Dimitris Zlatanidis <d.zlatanidis@gmail.com>
+# Copyright 2015-2019 Dimitris Zlatanidis <d.zlatanidis@gmail.com>
 # All rights reserved.
 
 # SBo tool for managing templates.
@@ -24,7 +24,7 @@
 
 __prog__ = "sbo-templates"
 __author__ = "dslackw"
-__copyright__ = 2015-2018
+__copyright__ = 2015-2019
 __version_info__ = (1, 0, 2)
 __version__ = "{0}.{1}.{2}".format(*__version_info__)
 __license__ = "GNU General Public License v3 (GPLv3)"
